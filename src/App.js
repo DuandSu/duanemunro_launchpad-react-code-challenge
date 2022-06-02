b9db67b5fce550a6faed60eb3f15ded612b7747b
DuandSu <duane.w.munro@gmail.com>
2022-06-02 13:20:54 -0600

    2-Jun-2022: Step1: Finished Very Basic Home Page. Expand in Step 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import HomePage from './components/HomePage';
 class App extends Component {
   render () {
     return(
-      <HomePage/>  
+      <div>
+        <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+          <link href="https://fonts.googleapis.com/css2?family=Tangerine:wght@400;700&display=swap" rel="stylesheet"></link>
+        </head>
+        <HomePage/>
+      </div>
     )
   }
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,21 +3,21 @@ import ItemList from './ItemList';
 function HomePage() {
     return (
       <div id="container">
+        <div id="nav">
+          <ul>
+            <a href="https://nulifenutritionyyc.ca/cannabis/">
+              <li className="siteLinks">Universities</li>
+            </a>
+            <a href="https://nulifenutritionyyc.ca/cannabis/">
+              <li className="siteLinks">Postal Lookup</li>
+            </a>
+          </ul>
+        </div>
         <div id="header">
           <h1>Duane Munro Launchpad React Code Challenge</h1>
         </div>
         <div id="content">
           <h1>Home Page - CRUD Operations</h1>
-          <div id="nav">
-            <ul>
-              <a href="https://nulifenutritionyyc.ca/cannabis/">
-                <li className="siteLinks">Universities</li>
-              </a>
-              <a href="https://nulifenutritionyyc.ca/cannabis/">
-                <li className="siteLinks">Postal Lookup</li>
-              </a>
-            </ul>
-          </div>
           <br></br>
           <table>
             <thead>
