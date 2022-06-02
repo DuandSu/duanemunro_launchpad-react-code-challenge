ca629b13c1109e0346c50289fcac2249b891154a
DuandSu <duane.w.munro@gmail.com>
2022-06-02 14:51:44 -0600

    2-Jun-2022: Step1: Basic Test of U & PL

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
 import HomePage from './components/HomePage';
+import Universities from './components/Universities';
+import PostalLookup from './components/PostalLookup';
 
 class App extends Component {
   render () {
+    let output = [];
+    output.push(<HomePage key="home"/>);
+    // output.push(<Universities key="universities"/>);
+    // output.push(<PostalLookup key="postal"/>);
     return(
       <div>
         <head>
@@ -11,7 +17,7 @@ class App extends Component {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
           <link href="https://fonts.googleapis.com/css2?family=Tangerine:wght@400;700&display=swap" rel="stylesheet"></link>
         </head>
-        <HomePage/>
+        {output}
       </div>
     )
   }
