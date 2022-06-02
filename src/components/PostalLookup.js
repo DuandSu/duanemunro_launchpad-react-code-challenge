751b21d1f5ad80440d9ab5f71d056d63c261b3c0
DuandSu <duane.w.munro@gmail.com>
2022-06-02 14:34:10 -0600

    2-Jun-2022: Step1: Finished Very Basic Universities & Postal Lookup Page. Expand in Step 2

--- src/components/PostalLookup.js
+++ src/components/PostalLookup.js
@@ -1,8 +1,24 @@
 function PostalLookup() {
     return (
-        <div>
-          <h1>Postal Lookup Page</h1>
+      <div id="container">
+        <div id="nav">
+          <ul>
+            <a href="https://nulifenutritionyyc.ca/cannabis/">
+              <li className="siteLinks">Home</li>
+            </a>
+            <a href="https://nulifenutritionyyc.ca/cannabis/">
+              <li className="siteLinks">Universities</li>
+            </a>
+          </ul>
         </div>
+        <div id="header">
+          <h1>Duane Munro Launchpad React Code Challenge</h1>
+        </div>
+        <div id="content">
+          <h1>Postal Lookup Page</h1>
+          <br></br>
+        </div>  
+      </div>
     );
 }
 
--- src/components/Universities.js
+++ src/components/Universities.js
@@ -1,8 +1,24 @@
 function Universities() {
     return (
-        <div>
-          <h1>Universities Page</h1>
+      <div id="container">
+        <div id="nav">
+          <ul>
+            <a href="https://nulifenutritionyyc.ca/cannabis/">
+              <li className="siteLinks">Home</li>
+            </a>
+            <a href="https://nulifenutritionyyc.ca/cannabis/">
+              <li className="siteLinks">Postal Lookup</li>
+            </a>
+          </ul>
         </div>
+        <div id="header">
+          <h1>Duane Munro Launchpad React Code Challenge</h1>
+        </div>
+        <div id="content">
+          <h1>Universities Page</h1>
+          <br></br>
+        </div>  
+      </div>
     );
 }
 
